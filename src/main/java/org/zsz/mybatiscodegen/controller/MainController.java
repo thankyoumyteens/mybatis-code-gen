84ea77965862b1615af4cbb442992ed89a96d670
zhaosz <iloveyesterday@outlook.com>
2023-06-17 21:36:33 +0800

    '1'

--- src/main/java/org/zsz/mybatiscodegen/controller/MainController.java
+++ src/main/java/org/zsz/mybatiscodegen/controller/MainController.java
@@ -1,47 +1,56 @@
 package org.zsz.mybatiscodegen.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.zsz.mybatiscodegen.entity.DbUsers;
 import org.zsz.mybatiscodegen.entity.GenParam;
-import org.zsz.mybatiscodegen.entity.GeneratorProperty;
-import org.zsz.mybatiscodegen.service.DbService;
-import org.zsz.mybatiscodegen.service.GenService;
+import org.zsz.mybatiscodegen.entity.Result;
 import org.zsz.mybatiscodegen.service.MainService;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/main")
 public class MainController {
 
     @Autowired
     MainService mainService;
 
+    @RequestMapping("/index")
+    public String index(){
+        return "index" ;
+    }
+
     @GetMapping
+    @ResponseBody
     @RequestMapping("/init")
-    public String init() {
+    public Result<String> init() {
         mainService.init();
-        return "1";
+        return Result.success();
     }
 
     @GetMapping
+    @ResponseBody
     @RequestMapping("/list")
-    public List<DbUsers> dbList() {
-        return mainService.dbList();
+    public Result<List<DbUsers>> dbList() {
+        List<DbUsers> dbUsersList = mainService.dbList();
+        return Result.success(dbUsersList);
     }
 
     @PostMapping
+    @ResponseBody
     @RequestMapping("/add")
-    public String add(@RequestBody DbUsers dbUsers) {
+    public Result<String> add(@RequestBody DbUsers dbUsers) {
         mainService.add(dbUsers);
-        return "1";
+        return Result.success();
     }
 
     @PostMapping
+    @ResponseBody
     @RequestMapping("/gen")
-    public String genMybatisCode(@RequestBody GenParam param) throws Exception {
+    public Result<String> genMybatisCode(@RequestBody GenParam param) throws Exception {
         mainService.genMybatisCode(param);
-        return "1";
+        return Result.success();
     }
 }
---
+++ src/main/java/org/zsz/mybatiscodegen/enums/ResultEnum.java
@@ -0,0 +1,25 @@
+package org.zsz.mybatiscodegen.enums;
+
+public enum ResultEnum {
+    /**
+     * 成功
+     */
+    SUCCESS(200),
+    /**
+     * 失败
+     */
+    FAIL(400),
+    /**
+     * 接口不存在
+     */
+    NOT_FOUND(404),
+    /**
+     * 服务器内部错误
+     */
+    INTERNAL_SERVER_ERROR(500);
+    public final int code;
+
+    ResultEnum(int code) {
+        this.code = code;
+    }
+}
